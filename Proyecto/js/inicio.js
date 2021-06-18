ce7741e1a47e1ecfa4a0941b0191030461c28a27
Dbcooper12 <marianolv11@gmail.com>
2021-06-18 18:35:49 -0500

    Arreglando codigo

---
+++ Proyecto/js/inicio.js
@@ -0,0 +1,14 @@
+function inicio() {
+    window.location="src/Carrito de compras.html";
+}
+
+function Proyecto1() {
+    window.location="proyecto1.html";
+}
+
+function Proyecto2() {
+    window.location="proyecto2.html";
+}
+function Proyecto3() {
+    window.location="proyecto3.html";
+}
