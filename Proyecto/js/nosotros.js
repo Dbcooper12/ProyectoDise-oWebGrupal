25bc10c6d6aca09600612f4708046c58a1d94bd1
ShirleyOrrego <shirleyoch.23@gmail.com>
2021-07-05 00:40:55 -0500

    nosotros

---
+++ Proyecto/js/nosotros.js
@@ -0,0 +1,15 @@
+let cerrar=document.querySelectorAll(".close")[0];
+let abrir=document.querySelectorAll(".mis")[0];
+let modal=document.querySelectorAll(".modal")[0];
+let modalC=document.querySelectorAll(".modal-container")[0];
+
+abrir.addEventListener("click",function(e){
+    e.preventDefault();
+    modalC.style.opacity = "1";
+    modalC.style.visibility = "visible";
+    modal.classList.toggle("modal-close");
+});
+
+cerrar.addEventListener("click",function(){
+    modal.classList.toggle("modal-close");
+})
